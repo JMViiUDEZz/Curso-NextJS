e97d90dd1ed695ec84e2223fc391e2d7ea115909
José María Viúdez <jmviudezp2003@gmail.com>
2022-09-19 18:35:59 +0200

    19

--- 01-initial-demo/pages/about.jsx
+++ 01-initial-demo/pages/about.jsx
@@ -1,34 +1,22 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+//import styles from '../styles/Home.module.css'
 import Link from 'next/link'
+import { MainLayout } from '../components/layouts/MainLayout'
 import { Navbar } from '../components/Navbar'
 
 export default function AboutPage() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>About - Jose</title>
-        <meta name="description" content="About Page" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <Navbar/>
-
-      <main className={styles.main}>
-
-        <h1>About Page</h1>
+    <MainLayout>
+      <h1>About Page</h1>
         
-        <h1 className={styles.title}>
+        <h1 className={"title"}>
           Ir a <Link href="/">Home</Link>
         </h1>
 
-        <p className={styles.description}>
+        <p className={"description"}>
           Editar{' '}
-          <code className={styles.code}>pages/about.js</code>
+          <code className={"code"}>pages/about.js</code>
         </p>
-
-      </main>
-
-    </div>
+    </MainLayout>
   )
 }
--- 01-initial-demo/pages/contact/index.jsx
+++ 01-initial-demo/pages/contact/index.jsx
@@ -1,34 +1,22 @@
 import Head from 'next/head'
-import styles from '../../styles/Home.module.css'
+//import styles from '../../styles/Home.module.css'
 import Link from 'next/link'
+import { MainLayout } from '../../components/layouts/MainLayout'
 import { Navbar } from '../../components/Navbar'
 
 export default function ContactPage() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Contact - Jose</title>
-        <meta name="description" content="Contact Page" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <Navbar/>
-
-      <main className={styles.main}>
-
-        <h1>Contact Page</h1>
+    <MainLayout>
+      <h1>Contact Page</h1>
         
-        <h1 className={styles.title}>
+        <h1 className={"title"}>
           Ir a <Link href="/">Home</Link>
         </h1>
 
-        <p className={styles.description}>
+        <p className={"description"}>
           Editar{' '}
-          <code className={styles.code}>pages/contact.js</code>
+          <code className={"code"}>pages/contact.js</code>
         </p>
-
-      </main>
-
-    </div>
+    </MainLayout>
   )
 }
