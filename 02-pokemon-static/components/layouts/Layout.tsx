32b94c18f430ed1c8a1c96f3aa155eaa048f7981
José María Viúdez <jmviudezp2003@gmail.com>
2022-11-01 13:59:35 +0100

    43

---
+++ 02-pokemon-static/components/layouts/Layout.tsx
@@ -0,0 +1,43 @@
+//Orden Importaciones:
+import React, { FC } from 'react' //1.React
+
+import Head from 'next/head' //2.Next
+
+//toma el archivo barril index.ts
+// import { Navbar } from '../ui'; //3.Terceros
+
+//4.Nuestras
+
+interface Props { 
+  children: React.ReactNode ;
+  title?: string;
+};
+
+
+// const origin = (typeof window === 'undefined') ? '' : window.location.origin;
+
+
+export const Layout: FC<Props> = ({ children, title }) => {
+  return (
+      <>
+        <Head>
+            <title>{ title || 'PokemonApp' }</title>
+            <meta name="author" content="JMViiUDEZz" />
+            <meta name="description" content={`Información sobre el pokémon ${ title }`} />
+            <meta name="keywords" content={ `${ title }, pokemon, pokedex`} />
+
+            {/* <meta property="og:title" content={`Información sobre ${ title }`} />
+            <meta property="og:description" content={`Esta es la página sobre ${ title }`} />
+            <meta property="og:image" content={`${ origin }/img/banner.png`} /> */}
+
+        </Head>
+      
+        {/* <Navbar /> */}
+
+        <main>
+            { children }
+        </main>
+      
+      </>
+  )
+};
--- 02-pokemon-static/pages/index.tsx
+++ 02-pokemon-static/pages/index.tsx
@@ -1,13 +1,14 @@
 import { Button } from '@nextui-org/react';
 import { NextPage } from 'next';
+import { Layout } from '../components/layouts';
 
 const HomePage: NextPage = () => {
   return (
-    <>
+    <Layout title='Listado de Pokemons'>
       <Button>
         Hola Mundo
       </Button>
-    </>
+    </Layout>
   )
 }
 
