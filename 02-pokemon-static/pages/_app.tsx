e5fce1b5795835e460e645020ab091ee304261d2
José María Viúdez <jmviudezp2003@gmail.com>
2022-11-01 11:02:07 +0100

    42

--- 02-pokemon-static/pages/_app.tsx
+++ 02-pokemon-static/pages/_app.tsx
@@ -1,9 +1,10 @@
 import type { AppProps } from 'next/app'
 
 // 1. import `NextUIProvider` component
-import { NextUIProvider } from '@nextui-org/react'
+import { NextUIProvider } from '@nextui-org/react';
+import { darkTheme } from '../themes';
 
-import '../styles/globals.css'
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   // 2. Use at the root of your app
@@ -12,7 +13,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     //por ello, podremos colocar elementos, objetos, funciones... y estarán disponibles en todos los hijos
     //(en pocas palabras, en casi toda la aplicacion)
     //en este caso, todos los hijos estan debajo de <Component/>
-    <NextUIProvider>
+    <NextUIProvider theme={ darkTheme }>
       <Component {...pageProps} />
     </NextUIProvider>
   )
